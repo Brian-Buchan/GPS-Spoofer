c51d8620ea83a5ec01156503e40cf11ebfd5a62d
buchanb5@ferris.edu <buchanb5@ferris.edu>
2018-10-11 13:29:57 -0400

    initial commit

--- GPS/GPS/Form1.Designer.cs
+++ GPS/GPS/Form1.Designer.cs
@@ -29,6 +29,22 @@
         private void InitializeComponent()
         {
             this.splitter1 = new System.Windows.Forms.Splitter();
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.btnTele = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Lat_Current = new System.Windows.Forms.TextBox();
+            this.txt_Long_Current = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Long_Select = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_Lat_Select = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_Phone_Name = new System.Windows.Forms.TextBox();
+            this.cmb_Connected_Devices = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // splitter1
@@ -39,21 +55,222 @@
             this.splitter1.TabIndex = 0;
             this.splitter1.TabStop = false;
             // 
+            // map
+            // 
+            this.map.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemmory = 5;
+            this.map.Location = new System.Drawing.Point(0, 0);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(658, 476);
+            this.map.TabIndex = 1;
+            this.map.Zoom = 0D;
+            // 
+            // btnTele
+            // 
+            this.btnTele.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTele.Location = new System.Drawing.Point(735, 166);
+            this.btnTele.Name = "btnTele";
+            this.btnTele.Size = new System.Drawing.Size(100, 23);
+            this.btnTele.TabIndex = 2;
+            this.btnTele.Text = "Jump to Location";
+            this.btnTele.UseVisualStyleBackColor = true;
+            this.btnTele.Click += new System.EventHandler(this.btnTele_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(684, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Latitude";
+            // 
+            // txt_Lat_Current
+            // 
+            this.txt_Lat_Current.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_Lat_Current.Location = new System.Drawing.Point(735, 25);
+            this.txt_Lat_Current.Name = "txt_Lat_Current";
+            this.txt_Lat_Current.Size = new System.Drawing.Size(100, 20);
+            this.txt_Lat_Current.TabIndex = 4;
+            // 
+            // txt_Long_Current
+            // 
+            this.txt_Long_Current.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_Long_Current.Location = new System.Drawing.Point(735, 51);
+            this.txt_Long_Current.Name = "txt_Long_Current";
+            this.txt_Long_Current.Size = new System.Drawing.Size(100, 20);
+            this.txt_Long_Current.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(675, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Longitude";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(741, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Current Location";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(741, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Current Location";
+            // 
+            // txt_Long_Select
+            // 
+            this.txt_Long_Select.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_Long_Select.Location = new System.Drawing.Point(735, 140);
+            this.txt_Long_Select.Name = "txt_Long_Select";
+            this.txt_Long_Select.Size = new System.Drawing.Size(100, 20);
+            this.txt_Long_Select.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(675, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Longitude";
+            // 
+            // txt_Lat_Select
+            // 
+            this.txt_Lat_Select.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_Lat_Select.Location = new System.Drawing.Point(735, 114);
+            this.txt_Lat_Select.Name = "txt_Lat_Select";
+            this.txt_Lat_Select.Size = new System.Drawing.Size(100, 20);
+            this.txt_Lat_Select.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(684, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Latitude";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(762, 234);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Phone";
+            // 
+            // txt_Phone_Name
+            // 
+            this.txt_Phone_Name.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_Phone_Name.Location = new System.Drawing.Point(664, 250);
+            this.txt_Phone_Name.Name = "txt_Phone_Name";
+            this.txt_Phone_Name.Size = new System.Drawing.Size(171, 20);
+            this.txt_Phone_Name.TabIndex = 14;
+            // 
+            // cmb_Connected_Devices
+            // 
+            this.cmb_Connected_Devices.FormattingEnabled = true;
+            this.cmb_Connected_Devices.Location = new System.Drawing.Point(714, 298);
+            this.cmb_Connected_Devices.Name = "cmb_Connected_Devices";
+            this.cmb_Connected_Devices.Size = new System.Drawing.Size(121, 21);
+            this.cmb_Connected_Devices.TabIndex = 15;
+            this.cmb_Connected_Devices.DropDown += new System.EventHandler(this.cmb_Connected_Devices_DropDown);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(734, 282);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(92, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Available Devices";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(847, 476);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cmb_Connected_Devices);
+            this.Controls.Add(this.txt_Phone_Name);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_Long_Select);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_Lat_Select);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_Long_Current);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Lat_Current);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTele);
+            this.Controls.Add(this.map);
             this.Controls.Add(this.splitter1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Splitter splitter1;
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.Button btnTele;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Lat_Current;
+        private System.Windows.Forms.TextBox txt_Long_Current;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Long_Select;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_Lat_Select;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_Phone_Name;
+        private System.Windows.Forms.ComboBox cmb_Connected_Devices;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- GPS/GPS/Form1.cs
+++ GPS/GPS/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -7,6 +8,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using iMobileDevice;
+using iMobileDevice.iDevice;
+using iMobileDevice.Lockdown;
 
 namespace GPS
 {
@@ -15,6 +23,90 @@ namespace GPS
         public Form1()
         {
             InitializeComponent();
+            NativeLibraries.Load();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            map.MapProvider = GMapProviders.GoogleMap;
+            map.DragButton = MouseButtons.Right;
+            map.MinZoom = 5;
+            map.MaxZoom = 100;
+            map.Zoom = 10;
+            map.MarkersEnabled = true;
+            // Position at my house
+            changeMapPosition(43.005174, -85.640573);
+            map.OnPositionChanged += Map_OnPositionChanged;
+        }
+
+        private void Map_OnPositionChanged(PointLatLng point)
+        {
+            txt_Lat_Select.Text = point.Lat.ToString();
+            txt_Long_Select.Text = point.Lng.ToString();
+        }
+
+        private void changeMapPosition(double latitude, double longitude)
+        {
+            map.Position = new PointLatLng(latitude, longitude);
+            txt_Lat_Current.Text = latitude.ToString();
+            txt_Long_Current.Text = longitude.ToString();
+            GMapMarker mark = new GMarkerGoogle(map.Position, GMarkerGoogleType.blue);
+            GMapOverlay markers = new GMapOverlay("markers");
+            markers.Markers.Add(mark);
+            map.Overlays.Clear();
+            map.Overlays.Add(markers);
+        }
+
+        private void btnTele_Click(object sender, EventArgs e)
+        {
+            changeMapPosition(map.Position.Lat, map.Position.Lng);
+        }
+
+        private void cmb_Connected_Devices_DropDown(object sender, EventArgs e)
+        {
+            cmb_Connected_Devices.Items.Clear();
+            ReadOnlyCollection<string> udids = GetDevices();
+            foreach (string udid in udids)
+            {
+                cmb_Connected_Devices.Items.Add(udid);
+            }
+        }
+
+        private ReadOnlyCollection<string> GetDevices()
+        {
+            ReadOnlyCollection<string> udids;
+            int count = 0;
+
+            var idevice = LibiMobileDevice.Instance.iDevice;
+            var lockdown = LibiMobileDevice.Instance.Lockdown;
+
+            var ret = idevice.idevice_get_device_list(out udids, ref count);
+
+            if (ret == iDeviceError.NoDevice)
+            {
+                // Not actually an error in our case
+                //return;
+            }
+
+            ret.ThrowOnError();
+
+            // Get the device name
+            foreach (var udid in udids)
+            {
+                iDeviceHandle deviceHandle;
+                idevice.idevice_new(out deviceHandle, udid).ThrowOnError();
+
+                LockdownClientHandle lockdownHandle;
+                lockdown.lockdownd_client_new_with_handshake(deviceHandle, out lockdownHandle, "Quamotion").ThrowOnError();
+
+                string deviceName;
+                lockdown.lockdownd_get_device_name(lockdownHandle, out deviceName).ThrowOnError();
+
+                deviceHandle.Dispose();
+                lockdownHandle.Dispose();
+            }
+
+            return udids;
         }
     }
 }
